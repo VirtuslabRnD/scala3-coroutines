17976696c2224ba97d0afef2622466f406ef35fa
Michał Pałka <prolativus@gmail.com>
2021-07-06 11:06:26 +0200

    Fix test for recursive coroutines

--- tests/run-cont/RecursiveCoroutines.scala
+++ tests/run-cont/RecursiveCoroutines.scala
@@ -1,22 +1,26 @@
 import scala.collection.*
 import scala.continuations.*
 
-// THIS TEST IS FAILING FOR NOW - INVESTIGATE WHY
-// probable cause: lack of eta-expansion during lifting
-
 @main def Test =
   val myEither = FailingWith[Double].run {
-    val input: List[Double] = (2.0 :: 4.0 :: 6.0 :: 8.0 :: 10.0 :: 0.3 :: 1.0 :: Nil)
-    println(rec(input))
+    val input: List[Double] = 2.0 :: 4.0 :: 6.0 :: 8.0 :: 10.0 :: 0.3 :: 1.0 :: Nil
+    rec(input)
   }
 
   val myEither2 = FailingWith[Double].run {
-    val input: List[Double] = (2.0 :: 4.0 :: 6.0 :: 8.0 :: 10.0 :: 0.0 :: 1.0 :: Nil)
-    println(rec(input))
+    val input: List[Double] = 2.0 :: 4.0 :: 6.0 :: 8.0 :: 10.0 :: 0.0 :: 1.0 :: Nil
+    rec(input)
   }
 
   println(myEither)
   println(myEither2)
 
 def rec(list: List[Double]): List[Int] FailWith Double =
-  if list == Nil then Nil else if list.head.isWhole then list.head.toInt :: rec(list.tail) else fail(list.head)
+  if list == Nil then Nil
+  else if list.head.isWhole then
+    val tail = list.tail
+    val recTail = rec(tail)
+    list.head.toInt :: recTail
+  else 
+    val head = list.head
+    fail(head)
